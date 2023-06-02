c7720271104544236d76cf769cc3004bb41f69b0
linhaorui <2909753663@qq.com>
2023-06-02 23:38:00 +0800

    start redux demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { createStore, Store, Action } from 'redux';
 
 import { AppComponent } from './app.component';
 import { ChatNavBarComponent } from './chat-nav-bar/chat-nav-bar.component';
 import { ChatThreadsComponent } from './chat-threads/chat-threads.component';
 import { ChatWindowComponent } from './chat-window/chat-window.component';
 import { FormsModule } from '@angular/forms';
+import { CounterComponent } from './counter/counter.component';
+import { AppState } from './store/app-state';
+import { counterReducer } from './store/counter/counter-reducer';
+import { AppStore } from './store/app-store';
+
+const store: Store<AppState> = createStore<AppState, Action, unknown, unknown>(
+  counterReducer
+);
 
 @NgModule({
   declarations: [
@@ -13,9 +22,15 @@ import { FormsModule } from '@angular/forms';
     ChatNavBarComponent,
     ChatThreadsComponent,
     ChatWindowComponent,
+    CounterComponent,
   ],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [
+    {
+      provide: AppStore,
+      useValue: store,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
